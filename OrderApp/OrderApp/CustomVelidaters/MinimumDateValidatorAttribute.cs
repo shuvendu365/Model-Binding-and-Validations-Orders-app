b2198f0d39aaf4550fe339644cefcd9bd1128a66
shuvendu365 <shuvendumohanta@gmail.com>
2023-05-03 17:01:00 +0530

    ShoppingApp

---
+++ OrderApp/OrderApp/CustomVelidaters/MinimumDateValidatorAttribute.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+
+public class MinimumDateValidatorAttribute : ValidationAttribute
+{
+    private readonly DateTime _minDate;
+
+    public MinimumDateValidatorAttribute(string minDate)
+    {
+        _minDate = DateTime.ParseExact(minDate, "yyyy-MM-dd", CultureInfo.InvariantCulture);
+        ErrorMessage = $"The field {{0}} must be greater than or equal to {_minDate:d}";
+    }
+
+    protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+    {
+        if (value == null)
+        {
+            return new ValidationResult(ErrorMessage);
+        }
+
+        if (!(value is DateTime))
+        {
+            throw new ArgumentException("Value must be a DateTime object");
+        }
+
+        DateTime dateValue = (DateTime)value;
+
+        if (dateValue < _minDate)
+        {
+            return new ValidationResult(ErrorMessage);
+        }
+
+        return ValidationResult.Success;
+    }
+}
---
+++ OrderApp/OrderApp/Models/Product.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace OrderApp.Models
+{
+    public class Product
+    {
+        [Required(ErrorMessage ="{0} Can't be blank")]
+        [Range(1,int.MaxValue,ErrorMessage ="{0} Shold be a valid number")]
+        [DisplayName("Product Code0")]
+        public int ProductCode { get; set; }
+        [Required(ErrorMessage = "{0} can't be blank")]
+        [Display(Name = "Product Price ")]
+        [Range(1, int.MaxValue, ErrorMessage = "{0} should be between a valid number")]
+        public double Price { get; set; }
+        [Required(ErrorMessage = "{0} can't be blank")]
+        [Display(Name = "Product Quantity")]
+        [Range(1, int.MaxValue, ErrorMessage = "{0} should be between a valid number")]
+        public int Quantity { get; set; }
+    }
+}
